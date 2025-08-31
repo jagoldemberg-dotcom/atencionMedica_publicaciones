70403582904db7a4a2da02f85aa1382ff2ceb31f
jagoldemberg-dotcom <ja.goldemberg@duocuc.cl>
2025-08-31 14:53:00 -0400

    Feat: se llenan listas de objetos

--- actividad2/src/main/java/com/example/actividad2/medical/service/MedicalCareService.java
+++ actividad2/src/main/java/com/example/actividad2/medical/service/MedicalCareService.java
@@ -20,14 +20,19 @@ public class MedicalCareService {
         patients.addAll(List.of(
                 new Patient(1L, "12.345.678-5", "Ana", "Pérez", LocalDate.of(1990, 3, 12), "+56 9 1234 5678", "ana.perez@example.com"),
                 new Patient(2L, "9.876.543-2", "Bruno", "Lagos", LocalDate.of(1985, 11, 5), "+56 9 2222 2222", "bruno.lagos@example.com"),
-                new Patient(3L, "7.654.321-K", "Carla", "Muñoz", LocalDate.of(2001, 7, 21), "+56 9 3333 3333", "carla.munoz@example.com")
+                new Patient(3L, "7.654.321-K", "Carla", "Muñoz", LocalDate.of(2001, 7, 21), "+56 9 3333 3333", "carla.munoz@example.com"),
+                new Patient(3L, "7.444.321-K", "Carlos", "Muñoz", LocalDate.of(2001, 7, 21), "+56 9 3333 3333", "carlos.munoz@example.com"),
+                new Patient(3L, "7.233.321-K", "Alejandra", "Muñoz", LocalDate.of(2001, 7, 21), "+56 9 3333 3333", "alejandra.munoz@example.com")
         ));
         visits.addAll(List.of(
                 new MedicalVisit(1L, 1L, LocalDate.now().minusDays(30), "Medicina General", "Rinitis alérgica", "Control anual"),
                 new MedicalVisit(2L, 1L, LocalDate.now().minusDays(10), "Dermatología", "Dermatitis", "Tratamiento tópico"),
                 new MedicalVisit(3L, 2L, LocalDate.now().minusDays(3), "Kinesiología", "Lumbalgia", "Ejercicios de estiramiento"),
                 new MedicalVisit(4L, 3L, LocalDate.now().minusDays(15), "Oftalmología", "Miopía", "Indicación de lentes"),
-                new MedicalVisit(5L, 3L, LocalDate.now().minusDays(1), "Medicina General", "Resfrío común", "Reposo 3 días")
+                new MedicalVisit(5L, 3L, LocalDate.now().minusDays(1), "Medicina General", "Resfrío común", "Reposo 3 días"),
+                new MedicalVisit(4L, 2L, LocalDate.now().minusDays(15), "Oftalmología", "Miopía", "Indicación de lentes"),
+                new MedicalVisit(4L, 1L, LocalDate.now().minusDays(15), "Dermatología", "Tinia", "Clotrimazol"),
+                new MedicalVisit(4L, 3L, LocalDate.now().minusDays(15), "Kinesiología", "Deformacion", "Terapia")
         ));
     }
 
